8564608eb9f259e1bb1df85e704ecdfa7fc59654
Ryan Skadberg <rskadberg@patientslikeme.com>
2018-08-21 12:20:10 -0400

    Catch up to other one

--- recipes/mod_ssl.rb
+++ recipes/mod_ssl.rb
@@ -16,7 +16,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-node.default['apache']['listen'] += ["*:#{node['apache']['mod_ssl']['port']}"]
+
+if node['apache']['listen'] == ['*:80']
+  node.default['apache']['listen'] += ["*:#{node['apache']['mod_ssl']['port']}"]
+end
 
 include_recipe 'apache2::default'
 
